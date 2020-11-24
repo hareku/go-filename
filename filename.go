93191f9b21cef05c6fe06992faca0654d88823c6
hareku <hareku908@gmail.com>
2020-11-24 09:40:53 +0000

    Improve EscapeString comment

--- filename.go
+++ filename.go
@@ -6,7 +6,8 @@ import "regexp"
 // reservedChars references https://en.wikipedia.org/wiki/Filename#Reserved_characters_and_words.
 var reservedChars = regexp.MustCompile(`[./<>|:&?%*"]`)
 
-// EscapeString escapes reserved characters in Unix and Windows.
+// EscapeString replaces all reserved characters in Unix and Windows with string repl.
+// Tge reserved characters matches [./<>|:&?%*"].
 func EscapeString(s string, repl string) string {
 	return reservedChars.ReplaceAllString(s, repl)
 }
